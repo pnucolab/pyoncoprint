5f70c569ed9413f6a9a49a3d10a8b342b1ed8d70
Jeongbin Park <jeongbin.park@charite.de>
2020-02-14 17:02:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import io
+import setuptools
+
+with io.open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyoncoprint",
+    version="0.1.0",
+    author="Jeongbin Park",
+    author_email="jeongbin.park@charite.de",
+    description="PyOncoPrint",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/pjb7687/pyoncoprint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+    ],
+    ext_modules = [module_utils],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ]
+)
